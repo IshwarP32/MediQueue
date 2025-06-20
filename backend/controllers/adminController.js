0f6b48d21e09448171f3edf6cf03310c7c6a6b14
Ishwar Patil <ishwarpatil8767@gmail.com>
2025-06-20 21:46:49 +0530

    Added asyncHandler, ApiError, ApiResponse and stopped .env from being tracked

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,21 +1,19 @@
-// API for adding doctor
-const addDoctor = async (req,res)=>{
-    try{
-        const {name,
-            email,
-            password,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address
-        } = req.body;
-        res.status(200).end("OK")
-    }
-    catch(error){
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-    }
-}
+// API for adding doctor
+const addDoctor = asyncHandler((req,res)=>{
+    const {name,
+           email,
+           password,
+           speciality,
+           degree,
+           experience,
+           about,
+           fees,
+           address
+        } = req.body;
+        res.status(200).json(new ApiResponse(200, {name,email}, "asyncHandler Working"))
+})
 
 export {addDoctor};
