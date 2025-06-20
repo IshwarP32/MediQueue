053e65b0cbb88dc75ef307f645e67c81b85c8f18
Manash Krit <manashkrit06@gmail.com>
2025-06-21 02:51:33 +0530

    navbar modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 const home = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500">Testing Home Page</h1>
-      testing
-    </div>
-  );
+  return <div></div>;
 };
 
 export default home;
