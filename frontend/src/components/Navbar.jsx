5ea238ccda5e5ecf6c10ebf2e14ab1449ce241f4
Piyush Kumar <piyushp6043@gmail.com>
2025-06-21 14:42:45 +0530

    Added header section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,18 +37,23 @@ const Navbar = () => {
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex-col gap-4 p-4">
                 <p
-                  // onClick={() => navigate("/my-profile")}
+                  onClick={() => navigate("/my-profile")}
                   className="hover:text-black cursor-pointer"
                 >
                   My Profile
                 </p>
                 <p
-                  // onClick={() => navigate("/my-appointments")}
+                  onClick={() => navigate("/my-appointments")}
                   className="hover:text-black cursor-pointer"
                 >
                   My Appointments
                 </p>
-                <p className="hover:text-black cursor-pointer">Logout</p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Logout
+                </p>
               </div>
             </div>
           </div>
