9e84e88f7f608cb297748bf0bbd09e170df8501b
Vighnesh Dharmale <vighneshdharmale333@gmail.com>
2025-06-26 14:17:00 +0530

    completed uer controller without adding razorpay info

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, loginUser, getProfile, updateProfile, bookAppointment } from '../controllers/userController.js'
+import { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment, paymentRazorpay, verifyRazorpay } from '../controllers/userController.js'
 import authUser from '../middleware/authUser.js'
 import upload from '../middleware/multer.js'
 
@@ -10,5 +10,9 @@ userRouter.post('/login', loginUser)
 userRouter.get('/get-profile', authUser, getProfile)
 userRouter.post('/update-profile', authUser, updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment)
+userRouter.get('/appointments', authUser, listAppointment)
+userRouter.post('/cancel-appointment', authUser, cancelAppointment)
+userRouter.post('/payment-razorpay', authUser, paymentRazorpay)
+userRouter.post('/verifyRazorpay', authUser, verifyRazorpay)
 
 export default userRouter
